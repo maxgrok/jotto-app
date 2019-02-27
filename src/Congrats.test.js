6775f06fe40d08f653970ef63778a1015c7aa931
Max Goodman <max.goodman@flatironschool.com>
2019-02-26 21:03:38 -0500

    adding tests and coding congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -26,9 +26,13 @@ test('renders without error', () =>{
 
 test('renders no text when `success` prop is false', ()=>{
     const wrapper = setup({success: false})
-    
+    const component = findByTestAttr(wrapper, "component-congrats")
+    expect(component.text()).toBe("");
 })
 
 test('renders non-empty congrats message when `success` prop is true', ()=>{
+    const wrapper = setup({success:true})
+    const message= findByTestAttr(wrapper, "congrats-message")
 
+    expect(message.text().length).not.toBe(0);
 })
