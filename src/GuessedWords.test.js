a6444e90fa15626fc227e4f4366177be6b272f54
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 07:16:47 -0500

    adding tests and beforeEach

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -28,13 +28,19 @@ test('does not throw warning with expected props', ()=>{
 
 //'describe' helps document the tests for anyone else looking at them
 describe('if there are no words guessed', ()=>{
+    let wrapper; 
+    beforeEach(()=>{
+        wrapper = setup({guessedWords: []})
+    })    
+    
     test('renders without error', ()=>{
         const wrapper = setup({ guessedWords: []})
         const component = findByTestAttr(wrapper, 'component-guessed-words')
         expect(component.length).toBe(1);
     });
     test('renders instructions to guess a word', ()=>{
-
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text().length).not.toBe(0)
     })
 });
 
