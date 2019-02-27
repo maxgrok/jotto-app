8970b9ff59d6636aa5828f95850cd6c6e3703d7f
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 07:42:28 -0500

    adding tests for GuessedWords Component"

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
 import Enzyme from 'enzyme';
-import EnzymeAdapter from 'react-redux';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,6 +9,29 @@ const GuessedWords = (props) =>{
             Try to guess the secret word! 
             </span>
         )
+    } else{
+        const guessedWordsRows= props.guessedWords.map((word, index)=>{
+            return (
+                <tr data-test="guessed-word" key={index}>
+                    <td>{word.guesedWord}</td>
+                    <td>{word.letterMatchCount}</td>
+                </tr>
+            )
+        })
+        content = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr><th>Guess</th><th>Matching Letters</th></tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordsRows}
+                    </tbody>
+                </table>
+            </div>
+
+        )
     }
     return (
         <div data-test="component-guessed-words">
